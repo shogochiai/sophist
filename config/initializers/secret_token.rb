989b2eb8b19dedd20c51294fb4250a9cdf1115ed
shogohciai <shogo1104@gmail.com>
2014-04-13 01:40:37 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sophist::Application.config.secret_key_base = 'b466881b765a1be5dff5cbb3b0507114858189a6d78f4ddb913082b3f18ec081e7abb16425ee8633c615f02c64902275d60c2e57e5bf72eb76946cd8ec3bae80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sophist::Application.config.session_store :cookie_store, key: '_sophist_session'
